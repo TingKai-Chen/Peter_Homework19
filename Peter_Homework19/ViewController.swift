fcfbe708cd19c6a8e308d1c17cd78566bcaea80f
“陳庭楷” <bill800123@gmail.com>
2019-01-02 13:35:27 +0800

    Initial Commit

---
+++ Peter_Homework19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peter_Homework19
+//
+//  Created by 陳庭楷 on 2019/1/2.
+//  Copyright © 2019 TingKai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
